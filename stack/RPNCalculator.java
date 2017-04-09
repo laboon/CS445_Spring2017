bf31517668ce993b7783e99411b77404ce6b1bed
laboon <laboon@gmail.com>
2017-04-09 16:31:52 -0400

    add RPN calc

--- stack/RPNCalculator.java
+++ stack/RPNCalculator.java
@@ -1,7 +1,15 @@
 public class RPNCalculator {
 
+    /**
+     * Our stack of values
+     */
+    
     SimpleStack s = new SimpleStack();
 
+    /**
+     * Apply "add" operation to top two values of stack"
+     */
+    
     public void add() {
 	int x = s.pop();
 	int y = s.pop();
@@ -9,13 +17,21 @@ public class RPNCalculator {
 	s.push(z);
     }
 
+    /**
+     * Apply "subtract" operation to top two values of stack"
+     */
+    
     public void subtract() {
 	int x = s.pop();
 	int y = s.pop();
 	int z = y - x;
 	s.push(z);
     }
-    
+
+    /**
+     * Apply "multiply" operation to top two values of stack"
+     */
+
     public void multiply() {
 	int x = s.pop();
 	int y = s.pop();
@@ -23,6 +39,10 @@ public class RPNCalculator {
 	s.push(z);
     }
 
+    /**
+     * Apply "divide" operation to top two values of stack"
+     */
+    
     public void divide() {
 	int x = s.pop();
 	int y = s.pop();
@@ -30,6 +50,21 @@ public class RPNCalculator {
 	s.push(z);
     }
 
+    /**
+     * Given some list of arguments from the command line, interpret
+     * them as an RPN list of operations and calculate them
+     * Possible arguments: +, -, x, /, or an integer value
+     * Example: "2 3 4 + x"
+     *   Creates a stack with values 2, 3, 4
+     *   Applies + to the top two elements (3, 4) = 7, 
+     *      then puts result (7) on stack
+     *   New stack is now 2, 7
+     *   Applies x to top two elements (2, 7) = 14,
+     *      then puts result (14) on stack
+     * @param args List of arguments
+     * SIDE EFFECT
+     */
+    
     public void calculate(String[] args) {
 	for (String x : args) {
 
@@ -39,7 +74,7 @@ public class RPNCalculator {
 		add();
 	    } else if (x.equals("-")) {
 		subtract();
-	    } else if (x.equals("*")) {
+	    } else if (x.equals("x")) {
 		multiply();
 	    } else if (x.equals("/")) {
 		divide();
